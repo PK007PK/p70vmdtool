29603ee687b7d1e9fe5b277f6018ff88c46212dc
Piotr Krasny <krasny.piotr@gmail.com>
2022-03-24 21:33:20 +0100

    Fix 2

--- src/lib/analizeString.js
+++ src/lib/analizeString.js
@@ -13,14 +13,8 @@ export const analizeString = () => {
 
         for (const element of searchingFor) {
             for (let i = 0; i < cleanedTextInTable.length; i++) {
-                console.log(element);
                 if (element === "IBAN" && element !== cleanedTextInTable[i + 1] && element === cleanedTextInTable[i]) {
-                    let ibanValue = "";
-                        for (let k = 1; k <= 8; k++) {
-                            console.log(ibanValue);
-                            if (cleanedTextInTable[i + k]) ibanValue = ibanValue + cleanedTextInTable[i + k];
-                        }
-                    valuesFound[element] = ibanValue;
+                    valuesFound[element] = cleanedTextInTable[i];
                 } else if (element === cleanedTextInTable[i]) {
                     valuesFound[element] = cleanedTextInTable[i + 1]
                 }
