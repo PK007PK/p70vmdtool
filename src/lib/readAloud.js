99907df1f48b26e3d87febb3ae4f90141e1d1c85
Piotr Krasny <krasny.piotr@gmail.com>
2022-04-04 22:51:50 +0200

    Speed up text speach

--- src/lib/readAloud.js
+++ src/lib/readAloud.js
@@ -1,6 +1,6 @@
 export const readAloud = (text) => {
     const msg = new SpeechSynthesisUtterance();
-    msg.rate = 0.4;
+    msg.rate = 0.7;
     const modifiedText = text.split('').join(' ');
     msg.text = modifiedText;
     window.speechSynthesis.speak(msg);
