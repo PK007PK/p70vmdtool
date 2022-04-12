c2ae1bc26fbb441be906f4d4988517b94ae00139
Piotr Krasny <krasny.piotr@gmail.com>
2022-04-12 22:24:11 +0200

    Smal fix

--- src/components/atoms/Summary/Summary.tsx
+++ src/components/atoms/Summary/Summary.tsx
@@ -25,62 +25,52 @@ export const Summary = () => {
        allBankRecords[1].bankAccount !== defElemValue && allBankRecords[0].bankAccount !== defElemValue ? 
        `Bank account in ${allBankRecords[0].documentType} is different from bank account in the confirmation document.
        ${allBankRecords[0].documentType}: ${allBankRecords[0].bankAccount}.
-       Document: ${allBankRecords[1].bankAccount}.
-       ` : ``
+       Document: ${allBankRecords[1].bankAccount}. ` : ``
 
     //    Most ugly code ever
     const docIbanCheck = allBankRecords[1].iban !== allBankRecords[0].iban && 
         allBankRecords[1].iban !== defElemValue && allBankRecords[0].iban !== defElemValue ? 
         `IBAN in ${allBankRecords[0].documentType} is different from IBAN in the confirmation document.
         ${allBankRecords[0].documentType}: ${allBankRecords[0].iban}.
-        Document: ${allBankRecords[1].iban}.
-        ` : ``
+        Document: ${allBankRecords[1].iban}. ` : ``
 
     //    Most ugly code ever
     const docSwiftCheck = allBankRecords[1].swift !== allBankRecords[0].swift && 
         allBankRecords[1].swift !== defElemValue && allBankRecords[0].swift !== defElemValue ? 
         `SWIFT code in ${allBankRecords[0].documentType} is different than document's SWIFT code.
         ${allBankRecords[0].documentType}: ${allBankRecords[0].swift}.
-        Document: ${allBankRecords[1].swift}.
-        ` : ``
+        Document: ${allBankRecords[1].swift}. ` : ``
 
     //    Most ugly code ever
     const crossSystemsAccountCheck = allBankRecords[2].bankAccount !== allBankRecords[0].bankAccount && 
         allBankRecords[2].bankAccount !== defElemValue && allBankRecords[0].bankAccount !== defElemValue ? 
         `Bank account in ${allBankRecords[0].documentType} is different from bank account in ${allBankRecords[2].documentType}.
         ${allBankRecords[0].documentType}: ${allBankRecords[0].bankAccount}.
-        ${allBankRecords[2].documentType}: ${allBankRecords[2].bankAccount}.
-        ` : ``
+        ${allBankRecords[2].documentType}: ${allBankRecords[2].bankAccount}. ` : ``
  
     //    Most ugly code ever
     const crossSystemsBankKeyCheck = allBankRecords[2].bankKey !== allBankRecords[0].bankKey && 
         allBankRecords[2].bankKey !== defElemValue && allBankRecords[0].bankKey !== defElemValue ? 
         `Bank key in ${allBankRecords[0].documentType} is different from bank key in ${allBankRecords[2].documentType}.
         ${allBankRecords[0].documentType}: ${allBankRecords[0].bankKey}.
-        ${allBankRecords[2].documentType}: ${allBankRecords[2].bankKey}.
-        ` : ``
+        ${allBankRecords[2].documentType}: ${allBankRecords[2].bankKey}. ` : ``
 
     //    Most ugly code ever
     const crossSystemsIbanCheck = allBankRecords[2].iban !== allBankRecords[0].iban && 
         allBankRecords[2].iban !== defElemValue && allBankRecords[0].iban !== defElemValue ? 
         `IBAN in ${allBankRecords[0].documentType} is different from IBAN in ${allBankRecords[2].documentType}.
         ${allBankRecords[0].documentType}: ${allBankRecords[0].iban}.
-        ${allBankRecords[2].documentType}: ${allBankRecords[2].iban}.
-        ` : ``
+        ${allBankRecords[2].documentType}: ${allBankRecords[2].iban}. ` : ``
 
     const crossSystemsSwiftCheck = allBankRecords[2].swift !== allBankRecords[0].swift && 
         allBankRecords[2].swift !== defElemValue && allBankRecords[0].swift !== defElemValue ? 
         `SWIFT in ${allBankRecords[0].documentType} is different from SWIFT in ${allBankRecords[2].documentType}.
         ${allBankRecords[0].documentType}: ${allBankRecords[0].swift}.
-        ${allBankRecords[2].documentType}: ${allBankRecords[2].swift}.
-        ` : ``
+        ${allBankRecords[2].documentType}: ${allBankRecords[2].swift}. ` : ``
 
-    const ccProblemReport = ccProblem ? `The company code is missing in the local system. Data will not be replicated to CFIN. The supplier does not receive payment.
-    ` : "";
-    const pbProblemReport = pbProblem ? `There is posting block in the local system. Data will not be replicated to CFIN. The supplier does not receive payment.
-    ` : "";
-    const replicationProblemReport = replicationProblem ? `For technical reasons, data replication did not take place. We will analyse the situation and if necessary JIRA will be set up.
-    ` : "";
+    const ccProblemReport = ccProblem ? `The company code is missing in the local system. Data will not be replicated to CFIN. The supplier does not receive payment. ` : "";
+    const pbProblemReport = pbProblem ? `There is posting block in the local system. Data will not be replicated to CFIN. The supplier does not receive payment. ` : "";
+    const replicationProblemReport = replicationProblem ? `For technical reasons, data replication did not take place. We will analyse the situation and if necessary JIRA will be set up. ` : "";
 
     const errorReport = `${ccProblemReport}${pbProblemReport}${replicationProblemReport}${docAccountCheck}${docIbanCheck}${docSwiftCheck}${crossSystemsBankKeyCheck}${crossSystemsAccountCheck}${crossSystemsIbanCheck}${crossSystemsSwiftCheck}`
 
