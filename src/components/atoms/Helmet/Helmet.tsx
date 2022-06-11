dbbac0848d3a619ffa9814390b2764c3e2719036
Piotr Krasny <krasny.piotr@gmail.com>
2022-06-11 14:21:33 +0200

    Add helmet

---
+++ src/components/atoms/Helmet/Helmet.tsx
@@ -0,0 +1,10 @@
+import { Helmet } from "react-helmet";
+
+export const HelmetComp = () => 
+    <Helmet>
+        <html lang="en" />
+        <title>IBAN defender. FVMD tool</title>
+        <meta name="description" content="Tool to facilitate manual bank data checks between financial systems and in supporting docs" />
+        <meta charSet="utf-8" />
+        <link rel="canonical" href="https://iban-defender.netlify.app/" />
+    </Helmet>
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
+import { HelmetComp } from 'components/atoms/Helmet/Helmet';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'views/App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <HelmetComp />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
