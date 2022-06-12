80d22fc1d78174af40e4a392e2380964b115122f
Piotr Krasny <krasny.piotr@gmail.com>
2022-06-12 09:43:16 +0200

    Improve OpenIban

--- src/components/atoms/OpenIban/OpenIban.tsx
+++ src/components/atoms/OpenIban/OpenIban.tsx
@@ -20,11 +20,12 @@ export const OpenIban = ({iban}: {iban: string}) => {
     const [data, setData] = useState<ApiResp | null>(null);
 
     useEffect(()=>{
+        console.log('check');
         (async (): Promise<void> => {
             try {
                 const res = await fetch(`https://openiban.com/validate/${iban}?getBIC=true&validateBankCode=true`);
-                const data: ApiResp = await res.json();
-                setData(data);
+                const respData: ApiResp = await res.json();
+                setData(respData);
             } catch(error) {
                 console.error(error);
             }
@@ -32,15 +33,18 @@ export const OpenIban = ({iban}: {iban: string}) => {
     }, [iban]);
 
     console.log(data);
-    if (!data) {
-        return <p>Loading data...</p>
+
+
+    if (data) {
+        return (
+            <OpenIbanStyle >
+                {
+                    iban === defElemValue ? null :
+                    <p>API: {data?.bankData?.bic ?? "No data in OpenIban"}</p>
+                }
+            </OpenIbanStyle>
+        )
     }
 
-    return (
-        <OpenIbanStyle >
-            {iban === defElemValue ?
-                null :
-                <p>API: {data?.bankData?.bic ?? "Cannot get BIC from OpenIban"}</p>}
-        </OpenIbanStyle>
-    )
+    return <></>
 }
