817bc803e65d2183c4184144a5057608b06652a9
Piotr Krasny <krasny.piotr@gmail.com>
2022-11-30 08:47:56 +0100

    OpenIban diseable

--- src/components/organisms/OneFinancialSystemData/OneFinancialSystemData.tsx
+++ src/components/organisms/OneFinancialSystemData/OneFinancialSystemData.tsx
@@ -1,5 +1,5 @@
 import { useContext, useMemo, useState } from "react";
-import { HiOutlineSwitchVertical } from 'react-icons/hi';
+import { HiOutlineSwitchVertical } from "react-icons/hi";
 
 import { AppContext } from "AppContext";
 
@@ -16,17 +16,16 @@ import { AcceptedDocuments } from "types/AcceptedDocuments";
 import { OneFinancialSystemDataStyle } from "./OneFinancialSystemData.style";
 
 interface Props {
-  main?: boolean,
-  systemNumber: number,
+  main?: boolean;
+  systemNumber: number;
 }
 
-export const OneFinancialSystemData: React.FunctionComponent<Props> = ({main, systemNumber}): JSX.Element | null=> {
-
-  const {
-       allBankRecords, 
-       forceUpdate,
-       changeMainFsBetweenRecords,
-      } = useContext(AppContext);
+export const OneFinancialSystemData: React.FunctionComponent<Props> = ({
+  main,
+  systemNumber,
+}): JSX.Element | null => {
+  const { allBankRecords, forceUpdate, changeMainFsBetweenRecords } =
+    useContext(AppContext);
 
   const bankRecord = allBankRecords[systemNumber];
 
@@ -35,66 +34,86 @@ export const OneFinancialSystemData: React.FunctionComponent<Props> = ({main, sy
   const handleReset = () => {
     bankRecord.reset();
     forceUpdate();
-  }
+  };
 
-  const TitleBar: React.FunctionComponent = (): JSX.Element =>       
+  const TitleBar: React.FunctionComponent = (): JSX.Element => (
     <div className="componentTitleBar">
       <div className="title">
         <h3>
-        {main && "Main fs: "}{bankRecord.documentType}
+          {main && "Main fs: "}
+          {bankRecord.documentType}
         </h3>
-        {bankRecord.documentType !== AcceptedDocuments.Document && 
-          <button 
+        {bankRecord.documentType !== AcceptedDocuments.Document && (
+          <button
             onClick={changeMainFsBetweenRecords}
-            title="Choose which financial system is main"  
+            title="Choose which financial system is main"
           >
             <HiOutlineSwitchVertical />
-          </button>}
+          </button>
+        )}
       </div>
       <div className="buttonsBar">
-        <Button onClick={handleReset} title="Reset current financial system">Reset</Button>
-        {bankRecord.documentType !== AcceptedDocuments.Document &&
-          <Button 
-            onClick={ ()=> pasteAll(bankRecord, forceUpdate)}
+        <Button onClick={handleReset} title="Reset current financial system">
+          Reset
+        </Button>
+        {bankRecord.documentType !== AcceptedDocuments.Document && (
+          <Button
+            onClick={() => pasteAll(bankRecord, forceUpdate)}
             title="Paste data from clipboard copied form fin sys, with kayword recognition"
           >
             OCR paste
           </Button>
-        }
-        {!main && <Button className="hideButton" onClick={()=>setIsVisible(false)}>X</Button>}
+        )}
+        {!main && (
+          <Button className="hideButton" onClick={() => setIsVisible(false)}>
+            X
+          </Button>
+        )}
       </div>
     </div>
+  );
 
-  const AllBankDetails: React.FunctionComponent = (): JSX.Element => 
+  const AllBankDetails: React.FunctionComponent = (): JSX.Element => (
     <>
       {Object.entries(bankRecord)
-        .filter(item => item[0] in BankRecordElements)
-        .map(([bankDetailsElementName,bankDetailsElementValue]) => {
-            if (bankRecord.documentType === AcceptedDocuments.Document && bankDetailsElementName === BankRecordElements.bankKey) {
-              return null;
-            }
-            if (bankRecord.documentType === AcceptedDocuments.Document && bankDetailsElementName === BankRecordElements.bankAccount) {
-              return null;
-            }
-            return (
-            <BankDetailsItem 
-              key={bankDetailsElementName} 
-              bankRecord={bankRecord}
-              bankDetailsElementName={bankDetailsElementName} 
-              bankDetailsElementValue={bankDetailsElementValue} 
-              handleClick={(e: React.MouseEvent<HTMLElement>) => pasteOneString(e, bankRecord, forceUpdate)}
-            />
-            )
+        .filter((item) => item[0] in BankRecordElements)
+        .map(([bankDetailsElementName, bankDetailsElementValue]) => {
+          if (
+            bankRecord.documentType === AcceptedDocuments.Document &&
+            bankDetailsElementName === BankRecordElements.bankKey
+          ) {
+            return null;
           }
-        )
-      }
+          if (
+            bankRecord.documentType === AcceptedDocuments.Document &&
+            bankDetailsElementName === BankRecordElements.bankAccount
+          ) {
+            return null;
+          }
+          return (
+            <BankDetailsItem
+              key={bankDetailsElementName}
+              bankRecord={bankRecord}
+              bankDetailsElementName={bankDetailsElementName}
+              bankDetailsElementValue={bankDetailsElementValue}
+              handleClick={(e: React.MouseEvent<HTMLElement>) =>
+                pasteOneString(e, bankRecord, forceUpdate)
+              }
+            />
+          );
+        })}
     </>
+  );
 
-  const openIbanBar = useMemo(()=> main &&
-    <div className="apiBar">
-      <OpenIban iban={bankRecord.iban}/>
-    </div>
-  ,[bankRecord.iban, main])
+  // const openIbanBar = useMemo(
+  //   () =>
+  //     main && (
+  //       <div className="apiBar">
+  //         <OpenIban iban={bankRecord.iban} />
+  //       </div>
+  //     ),
+  //   [bankRecord.iban, main]
+  // );
 
   if (!isVisible) {
     return null;
@@ -104,6 +123,7 @@ export const OneFinancialSystemData: React.FunctionComponent<Props> = ({main, sy
     <OneFinancialSystemDataStyle>
       <TitleBar />
       <AllBankDetails />
-      {openIbanBar}
+      {/* {openIbanBar} */}
     </OneFinancialSystemDataStyle>
-  )}
+  );
+};
--- src/types/AcceptedDocuments.ts
+++ src/types/AcceptedDocuments.ts
@@ -1,5 +1,5 @@
 export enum AcceptedDocuments {
     Sap = "SAP",
     Cfin = "CFIN",
-    Document = "Vendors document", 
+    Document = "Supporting doc", 
 }
